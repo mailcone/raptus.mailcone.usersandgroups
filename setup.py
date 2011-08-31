9a6cc5f52fba0de59737f6d2b8eeb949effd039a
samuelriolo <samuelriolo@2114add4-67ca-9ca6-10be-5e31bf341269>
2011-08-31 20:34:09 +0000

    add new packages

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a1'
+
+setup(name='raptus.mailcone.usersandgroups',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Samuel Riolo',
+      author_email='sriolo@raptus.com',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['raptus.mailcone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'fanstatic',
+          'zope.fanstatic',
+          'js.jquery',
+          'js.jqueryui',
+          'megrok.navigation',
+          'megrok.form',
+
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
